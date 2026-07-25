d2675a9e77ea53d6448620db66c39b00187c6cde
nikit <nnn479516@gmail.com>
2026-07-25 21:56:56 +0300

    обновление класса фигур

--- src/Piece.kt
+++ src/Piece.kt
@@ -1,39 +1,54 @@
 enum class Color { RED, BLACK }
 sealed class Piece(val color: Color) {
-    abstract fun getPseudoLegalMoves(from: Position, board: Board)
+    abstract fun getPseudoLegalMoves(from: Position, board: Board): List<Position>
 }
-class General (color: Color): Piece(color){
-    override fun getPseudoLegalMoves(from: Position, board: Board) {
 
+class General (color: Color): Piece(color){
+    override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
+        val validMoves = mutableListOf<Position>()
+
+        return validMoves.toList()
     }
 }
 class Advisor  (color: Color): Piece(color){
-    override fun getPseudoLegalMoves(from: Position, board: Board) {
-
+    override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
+        val validMoves = mutableListOf<Position>()
+        return validMoves.toList()
     }
 }
 class Elephant  (color: Color): Piece(color){
-    override fun getPseudoLegalMoves(from: Position, board: Board) {
-
+    override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
+        val validMoves = mutableListOf<Position>()
+        return validMoves.toList()
     }
 }
 class Horse  (color: Color): Piece(color){
-    override fun getPseudoLegalMoves(from: Position, board: Board) {
-
+    override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
+        val validMoves = mutableListOf<Position>()
+        return validMoves.toList()
     }
 }
 class Chariot  (color: Color): Piece(color){
-    override fun getPseudoLegalMoves(from: Position, board: Board) {
-
+    override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
+        val validMoves = mutableListOf<Position>()
+        val directions = listOf(
+            Position(1, 0),
+            Position(0, -1),
+            Position(-1, 0),
+            Position(0, 1),
+        )
+        return validMoves.toList()
     }
 }
 class Cannon  (color: Color): Piece(color){
-    override fun getPseudoLegalMoves(from: Position, board: Board) {
-
+    override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
+        val validMoves = mutableListOf<Position>()
+        return validMoves.toList()
     }
 }
 class Soldier  (color: Color): Piece(color){
-    override fun getPseudoLegalMoves(from: Position, board: Board) {
-
+    override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
+        val validMoves = mutableListOf<Position>()
+        return validMoves.toList()
     }
 }
