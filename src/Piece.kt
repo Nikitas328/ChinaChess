53b2021ccbbafca422e65b38a78c35db20038320
nikit <nnn479516@gmail.com>
2026-07-29 19:55:39 +0300

    генерал и служащий.все псевдолегальные ходы реализованы

--- src/Piece.kt
+++ src/Piece.kt
@@ -7,13 +7,63 @@ sealed class Piece(val color: Color) {
 class General (color: Color): Piece(color){
     override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
         val validMoves = mutableListOf<Position>()
+        val directions = listOf(
+            Position(1, 0),
+            Position(0, -1),
+            Position(-1, 0),
+            Position(0, 1),
+        )
+        for (direction in directions){
 
+            val step = Position(from.x + direction.x, from.y + direction.y)
+
+            if(!step.isValidToBoard()){continue}
+            if(color == Color.RED && (step.x !in 3..5  || step.y !in 0..2)){continue}
+            if(color == Color.BLACK && (step.x !in 3..5  || step.y !in 7..9)){continue}
+
+
+            val targetPiece = board.getPiece(step)
+            if(targetPiece == null){
+                validMoves.add(step)
+            } else {
+                if(targetPiece.color != this.color){
+                    validMoves.add(step)
+                }
+
+            }
+
+        }
         return validMoves.toList()
     }
 }
 class Advisor  (color: Color): Piece(color){
     override fun getPseudoLegalMoves(from: Position, board: Board): List<Position> {
         val validMoves = mutableListOf<Position>()
+        val directions = listOf(
+            Position(1, 1),
+            Position(1, -1),
+            Position(-1, -1),
+            Position(-1, 1),
+        )
+        for (direction in directions){
+
+            val step = Position(from.x + direction.x, from.y + direction.y)
+
+            if(!step.isValidToBoard()){continue}
+            if(color == Color.RED && (step.x !in 3..5  || step.y !in 0..2)){continue}
+            if(color == Color.BLACK && (step.x !in 3..5  || step.y !in 7..9)){continue}
+
+            val targetPiece = board.getPiece(step)
+            if(targetPiece == null){
+                validMoves.add(step)
+            } else {
+                if(targetPiece.color != this.color){
+                    validMoves.add(step)
+                }
+
+            }
+
+        }
         return validMoves.toList()
     }
 }
@@ -186,7 +236,6 @@ class Soldier  (color: Color): Piece(color){
                 if(targetPiece.color != this.color){
                     validMoves.add(step)
                 }
-                continue
             }
 
         }
